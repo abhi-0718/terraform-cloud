a0f0e3ec7a664f130d1a2e3e099264ef7410760c
Abhishek Dubey <Abhishek.Dubey2@citiustech.com>
2023-05-10 12:37:32 +0530

    Initial commit

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "ami" {
+  
+}
+
+variable "region" {
+  
+}
+
+variable "instance_type" {
+  
+}
+
+variable "access_key" {
+  
+}
+
+variable "secret_key" {
+  
+}
+
+variable "subnet_id" {
+  
+}
